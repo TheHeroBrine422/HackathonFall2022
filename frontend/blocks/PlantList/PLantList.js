e0a6d24ecf7ac3222526a7b0f618f49832e68b72
pplppyukp <47014345+saeagleson@users.noreply.github.com>
2022-11-12 07:28:31 -0600

    plant list

---
+++ frontend/blocks/PlantList/PLantList.js
@@ -0,0 +1,35 @@
+
+import { Grid } from "../../views/Home/components";
+
+const PlantList = ({}) => {
+return(
+  <Grid classSet="plantsGrid" rows={1} columns={4}>
+      <button>Plant 1</button>
+      <button>Plant 2</button>
+      <button>Plant 3</button>
+      <button>Plant 4</button>
+      <button>Plant 5</button>
+      <button>Plant 6</button>
+      <button>Plant 7</button>
+      <button>Plant 8</button>
+      <button>Plant 9</button>
+      <button>Plant 10</button>
+      <button>Plant 11</button>
+      <button>Plant 12</button>
+      <button>Plant 13</button>
+      <button>Plant 14</button>
+      <button>Plant 15</button>
+      <button>Plant 16</button>
+      <button>Plant 17</button>
+      <button>Plant 18</button>
+      <button>Plant 19</button>
+      <button>Plant 20</button>
+      <button>Plant 21</button>
+
+  </Grid>
+
+);
+
+}
+
+export default PlantList
--- frontend/pages/AddPlant/index.js
+++ frontend/pages/AddPlant/index.js
@@ -1,3 +1,4 @@
+import PlantList from "../../blocks/PlantList"
 import { SiteHeader } from "../../views/Home/components"
 
 const AddPlant = ({}) => {
@@ -5,6 +6,8 @@ const AddPlant = ({}) => {
     <div>
       <SiteHeader loggedIN={true} />
         <div className='pageContent'>
+          <button>Add New Plant Name</button>
+          <PlantList/>
           <button>
             Add a new plant
           </button>
