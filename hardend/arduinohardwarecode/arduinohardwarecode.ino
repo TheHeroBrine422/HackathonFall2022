70d1bd46dedb0f5f34c52a11b4179f17ea50bf9a
gaberay2022 <112833778+gaberay2022@users.noreply.github.com>
2022-11-12 04:43:42 -0600

    Add files via upload

--- hardend/arduinohardwarecode/arduinohardwarecode.ino
+++ hardend/arduinohardwarecode/arduinohardwarecode.ino
@@ -12,9 +12,16 @@ DHT dht(DHTPIN, DHTTYPE);
 
 
 int status = WL_IDLE_STATUS;
+int button = 0;
+int val;
 
 void setup() {
   Serial.begin(115200);
+
+  pinMode(18,OUTPUT);
+  pinMode(19,OUTPUT);
+  pinMode(21,OUTPUT);
+  pinMode(22,INPUT);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin("Caleb iphone", "hackathon");
@@ -25,13 +32,12 @@ void setup() {
   }  
 
   Serial.println("Connected!");
-  dht.begin();
-//Pin Setup Here  
+  dht.begin(); 
 }
 
 void loop() {
-  long Timer = 5000;
-
+  long Timer = 1000;
+  val = digitalRead(22);
   //server stuff
   delay(Timer);
   WiFiClient client;
@@ -41,6 +47,13 @@ void loop() {
   String sssid(ssid); //Gives the ssid identifier of the ESP32
   http.begin(client, "http://172.20.10.12:3000/plantAPI/sendPlantData");
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  if(val == LOW){
+    button = 1;
+  }
+  else{
+    button = 0;
+  }
 
   //Declare the Data Variables, and get the Data
   int Temp = dht.readTemperature(true);
@@ -53,15 +66,31 @@ void loop() {
   int Water;
   String waterData = String(Water);
   int Ph;
-  String phData = String(Ph);    
+  String phData = String(Ph);
+  String pairing = String(button);
+
+  Serial.println(sunData + "\n"); 
   Serial.print(F(" Humidity: "));
   Serial.print(humidityData);
   Serial.print(F("%  Temperature: "));
   Serial.print(temperatureData);
   Serial.print(F("°F "));
 
+  if(Sun < 600){
+    digitalWrite(18,HIGH);
+    digitalWrite(19,HIGH);
+    digitalWrite(21,HIGH);
+  }  
+  else{
+    digitalWrite(18,LOW);
+    digitalWrite(19,LOW);
+    digitalWrite(21,LOW);
+  }
+ 
+
+  Serial.println(pairing);
   //sever stuff
-  String PlantData = ("identifier=" + sssid + "&temperature=" + temperatureData + "&sun=" + sunData + "&humidity=" + humidityData + "&water=" + waterData + "&ph=" + phData);
+  String PlantData = ("identifier=" + sssid + "&temperature=" + temperatureData + "&sun=" + sunData + "&humidity=" + humidityData + "&water=" + waterData + "&ph=" + phData + "&pairing" + pairing);
   http.POST(PlantData);
   http.end();
 } 
