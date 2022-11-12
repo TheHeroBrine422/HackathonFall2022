791d5b565e14ecec214aeaa282c8f00f2fe724c4
Xaffels <jpharris68@gmail.com>
2022-11-12 01:37:52 -0600

    Fixed Hardware POST request, updates data

--- hardend/arduinohardwarecode/arduinohardwarecode.ino
+++ hardend/arduinohardwarecode/arduinohardwarecode.ino
@@ -11,42 +11,40 @@ void setup() {
   while(WiFi.status() != WL_CONNECTED){
     delay(500);   
     Serial.println(WiFi.status());
+      Serial.println("Connecting...");
   }  
   Serial.println("Connected!");
-//Pin Setup Here  
 }
 
 void loop() {
   long Timer = 5000;
   long PrevTimer = 0;
-  if((millis() - PrevTimer > Timer))
-  {
-    // WiFiClient client;
-    // HTTPClient http;
-    // char ssid[13];
-    // snprintf(ssid, 13, "%llX", ESP.getEfuseMac());
-    // String sssid(ssid); //Gives the ssid identifier of the ESP32
-    // http.begin(client, "http://192.168.137.1:3000/PlantAPI/sendPlantData");
-    // http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  //if((millis() - PrevTimer > Timer))
+  //{
+    delay(Timer);
+    WiFiClient client;
+    HTTPClient http;
+    char ssid[13];
+    snprintf(ssid, 13, "%llX", ESP.getEfuseMac());
+    String sssid(ssid); //Gives the ssid identifier of the ESP32
+    http.begin(client, "http://172.20.10.12:3000/plantAPI/sendPlantData");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
     //Declare the Data Variables, and get the Data
     int Temp;
     String temperatureData = String(Temp);
-    int Sun = analogRead(13);
+    int Sun = analogRead(33);
     String sunData = String(Sun);
-    Serial.println(analogRead(13));
+    Serial.println(sunData);
     int Humid;
     String humidityData = String(Humid);
     int Water;
     String waterData = String(Water);
     int Ph;
     String phData = String(Ph);    
-    //String PlantData = ("identifier=" + sssid + "&temperature=" + temperatureData + "&sun=" + sunData + "&humidity=" + humidityData + "&water=" + waterData + "&ph=" + phData);
-    // http.POST(PlantData);
-    // http.end();
-  }
-  PrevTimer = millis();
-   // Serial.println(analogRead(13));
-    // delay(200);
-
+    String PlantData = ("identifier=" + sssid + "&temperature=" + temperatureData + "&sun=" + sunData + "&humidity=" + humidityData + "&water=" + waterData + "&ph=" + phData);
+    http.POST(PlantData);
+    http.end();
+    PrevTimer = millis();
+  //}
 } 
