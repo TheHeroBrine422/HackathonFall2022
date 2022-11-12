715a859cd1bae9009c7d61555eb2730fc0bc26e4
Hero <caleb@calebgj.io>
2022-11-12 03:26:21 -0600

    backend been a while

--- backend/test.js
+++ backend/test.js
@@ -7,7 +7,7 @@ async function sendPlantData() {
     URLParams.append("humidity", 3)
     URLParams.append("water", 4)
     URLParams.append("ph", 5)
-    URLParams.append("identifier", "ABCDID")
+    URLParams.append("identifier", "1234ID")
 
     await axios.post("http://localhost:3000/plantAPI/sendPlantData", URLParams)
         .then(function (response) {
@@ -17,8 +17,9 @@ async function sendPlantData() {
 
 async function setPlantName() {
     const URLParams = new URLSearchParams();
-    URLParams.append("name", "Cactus Left")
-    URLParams.append("identifier", "ABCDID")
+    URLParams.append("token", "f7212028c121275f32cf9d2aad77dc0d88afbbc47b20af686ea636aea10006df98157ba1f3d021dd32dda66055ed87b1e8a7f69772fc9bd114ef5b665dc7c16d")
+    URLParams.append("name", "Flower Right")
+    URLParams.append("identifier", "3485A29EF0C8")
 
     await axios.post("http://localhost:3000/plantAPI/setPlantName", URLParams)
         .then(function (response) {
@@ -26,10 +27,43 @@ async function setPlantName() {
         })
 }
 
+async function register() {
+    const URLParams = new URLSearchParams();
+    URLParams.append("email", "caleb@calebgj.io")
+    URLParams.append("password", "testpassword")
+
+    await axios.post("http://localhost:3000/plantAPI/register", URLParams)
+        .then(function (response) {
+            console.log(response.data);
+        })
+}
+
+async function login() {
+    const URLParams = new URLSearchParams();
+    URLParams.append("email", "caleb@calebgj.io")
+    URLParams.append("password", "testpassword")
+
+    await axios.post("http://localhost:3000/plantAPI/login", URLParams)
+        .then(function (response) {
+            console.log(response.data);
+        })
+}
+
+async function faillogin() {
+    const URLParams = new URLSearchParams();
+    URLParams.append("email", "caleb@calebgj.io")
+    URLParams.append("password", "incorrect")
+
+    await axios.post("http://localhost:3000/plantAPI/login", URLParams)
+        .then(function (response) {
+            console.log(response.data);
+        })
+}
+
 async function setPlantType() {
     const URLParams = new URLSearchParams();
-    URLParams.append("type", "Cactus")
-    URLParams.append("identifier", "ABCDID")
+    URLParams.append("type", "Flower")
+    URLParams.append("identifier", "1234ID")
 
     await axios.post("http://localhost:3000/plantAPI/setPlantType", URLParams)
         .then(function (response) {
@@ -48,11 +82,17 @@ async function deletePlant() {
 }
 
 (async () => {
-    console.log("plantdata")
-    await sendPlantData()
-    console.log("name")
+    //console.log("plantdata")
+    //await sendPlantData()
+    //console.log("name")
     await setPlantName()
-    console.log("type")
-    await setPlantType()
+    //console.log("type")
+    //await setPlantType()
+    console.log("register")
+    //await register()
+    console.log("login")
+    //await login()
+    console.log("faillogin")
+    //await faillogin()
     //await deletePlant()
 })();
