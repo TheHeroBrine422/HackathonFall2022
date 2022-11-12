29464c0f7f58f7448016ec3cd785fbe399606ec0
Caleb Jones <caleb@calebgj.io>
2022-11-12 07:26:11 -0600

    add functionality for delete and rename plant on Plant.js

--- frontend/pages/Plants/Plants.js
+++ frontend/pages/Plants/Plants.js
@@ -3,6 +3,31 @@ import { SiteHeader, Typography } from '../../views/Home/components'
 import { useEffect, useState } from 'react'
 import {Grid } from '../../views/Home/components'
 
+function deletePlant() {
+  if (confirm("Do you really want to delete this plant?")) {
+    const URLParams = new URLSearchParams();
+    URLParams.append("identifier", localStorage.getItem('activePlant'))
+    URLParams.append('token', localStorage.getItem('token'))
+
+    axios.post("http://localhost:3000/plantAPI/deletePlant", URLParams)
+        .then(function (response) {
+          console.log(response.data);
+        })
+  }
+}
+
+function renamePlant() {
+  name = prompt("What do you want the new name of this plant to be?", data[localStorage.getItem('activePlant')].name)
+  const URLParams = new URLSearchParams();
+  URLParams.append("identifier", localStorage.getItem('activePlant'))
+  URLParams.append('token', localStorage.getItem('token'))
+  URLParams.append('name', name)
+
+  axios.post("http://localhost:3000/plantAPI/setPlantName", URLParams)
+      .then(function (response) {
+        console.log(response.data);
+      })
+}
 
 const Plants = ({data}) => {
 
@@ -12,7 +37,6 @@ const Plants = ({data}) => {
     setPlantData(data[localStorage.getItem('activePlant')])
   })
 
-
     return (
     <div>
       <SiteHeader loggedIN={true} />
@@ -20,9 +44,9 @@ const Plants = ({data}) => {
           <div className='homePortfolio'>
             <div>
               <Typography classSet={'homeHeadline'}>{plantData.name}</Typography>
-              <button>Rename</button>
+              <button onClick={renamePlant()}>Rename</button>
               <button>Change Plant</button>
-              <button>Remove Plant</button>
+              <button onClick={deletePlant()}>Remove Plant</button>
               <Grid classSet="plantPropertyGrid" rows={2} columns={3}>
               <Typography classSet="plantProperty humidity">Humidity: {data[localStorage.getItem('activePlant')].currentData.humidity}</Typography>
               <Typography classSet="plantProperty ph">PH: {data[localStorage.getItem('activePlant')].currentData.ph}</Typography>
